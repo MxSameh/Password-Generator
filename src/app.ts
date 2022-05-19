d5d215f1d3bcc67b516f10648488b8e4e897d454
Mohamed Sameh <mail.msameh@gmail.com>
2022-05-19 20:20:42 +0200

    password generator function is completed

---
+++ src/app.ts
@@ -0,0 +1,46 @@
+// START
+
+const generatePassword = (length: number) => {
+  //
+  const symbols = '!@#$%^&';
+  const numbers = '0123456789';
+  const capital = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  const small = capital.toLowerCase();
+  let password = '';
+  //
+  for (let i = 0; i < length; i++) {
+    let index: number;
+    let c: string;
+    const x: number = Math.round(Math.random() * 3);
+
+    switch (x) {
+      case 0:
+        index = Math.round(Math.random() * (symbols.length - 1));
+        c = symbols[index];
+        password += c;
+        break;
+      case 1:
+        index = Math.round(Math.random() * (capital.length - 1));
+        c = capital[index];
+        password += c;
+        break;
+      case 2:
+        index = Math.round(Math.random() * (small.length - 1));
+        c = small[index];
+        password += c;
+        break;
+      case 3:
+        index = Math.round(Math.random() * (numbers.length - 1));
+        c = numbers[index];
+        password += c;
+        break;
+    }
+  }
+  return password;
+};
+
+//********** FOR TESTING *****************//
+// for (let i = 0; i < 10 ; i++){
+//  let password = generatePassword(10);
+//  console.log(`${password.length} : ${password} ` );
+// }
